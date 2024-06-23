2ce09bf612bb320459a67d74ddb6605ddd0701d0
Pratik Pimpalkar <pratik@xerocodee.com>
2024-06-23 22:09:18 +0530

    fix bug

--- client/components/Trip/index.js
+++
@@ -1,1 +0,0 @@
-export { default as TripPlanner } from './TripPlanner'
--- server/places/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('api/search-place/', autoCompletePlaceSearch.as_view()),
-    path('api/get-place-details/', getPlaceDetails.as_view())
-]
